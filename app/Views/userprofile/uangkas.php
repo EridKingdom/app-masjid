4933b55c99beea4a5165d4dc4064315334031e5b
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2024-07-05 23:00:24 +0700

    fix uang kas masjid empty data

--- app/Views/userprofile/uangkas.php
+++ app/Views/userprofile/uangkas.php
@@ -297,7 +297,7 @@ if ($id_user) {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="addForm" method="POST" action="/uang/handleFormData/<?= $masjid['id_masjid'] ?>">
+                <form id="addForm" method="POST" action="/uang/handleFormData/<?= $masjid['id'] ?>">
                     <div class="mb-3">
                         <label for="tgl" class="form-label">Tanggal</label>
                         <input type="date" class="form-control" id="tgl" name="tgl" required>
